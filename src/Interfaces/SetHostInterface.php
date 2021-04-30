cbce49220fd8dc69d5845c2aeae20b32c4dda56f
hahadu <mhyzhu@163.com>
2021-04-30 16:02:05 +0800

    update

---
+++ src/Interfaces/SetHostInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Hahadu\ThinkCrawling\Interfaces;
+
+
+interface SetHostInterface
+{
+    /****
+     * 设置采集服务
+     * @param string $hostType
+     * @return void
+     */
+    public function setHostType(string $hostType);
+
+
+}
