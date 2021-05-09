c66baceba8790718bf729fe2fd6f9b90f1520620
hahadu <mhyzhu@163.com>
2021-05-10 00:31:59 +0800

    update

--- src/Crawling.php
+++ src/Crawling.php
@@ -5,9 +5,15 @@ namespace Hahadu\ThinkCrawling;
 
 
 use Hahadu\ThinkCrawling\Core\Base;
+use think\swoole\Websocket;
 
 class Crawling extends Base
 {
+    /*****
+     * @var Websocket
+     */
+    protected $websocket = false;
+
     protected $host = '';
 
     /****
@@ -15,6 +21,20 @@ class Crawling extends Base
      */
     public function setHost($host=''){
         $this->host = $host;
+    }
+
+    /****
+     * @param Websocket $websocket
+     */
+    public function setWebsocket($websocket){
+        $this->websocket = $websocket;
+    }
+
+    /******
+     * @return bool
+     */
+    public function isSocket(){
+        return (strtolower(php_sapi_name())=='cli' && ($this->websocket instanceof Websocket));
     }
 
 }
